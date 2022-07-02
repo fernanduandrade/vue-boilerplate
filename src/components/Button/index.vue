c1be32d0191a5e15c44228c85229b8b18ef434db
fernanduandrade <onanduandrade@gmail.com>
2022-07-02 16:37:15 -0300

    remove: storybook

---
+++ src/components/Button/index.vue
@@ -0,0 +1,48 @@
+<script lang="ts">
+import './scss/base.scss';
+import { reactive, computed, defineComponent } from 'vue';
+export default defineComponent({
+  name: 'Button',
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    primary: {
+      type: Boolean,
+      default: false,
+    },
+    size: {
+      type: String,
+      validator: function (value: string) {
+        return ['small', 'medium', 'large'].indexOf(value) !== -1;
+      },
+    },
+    backgroundColor: {
+      type: String,
+    },
+  },
+  emits: ['click'],
+  setup(props, { emit }) {
+    props = reactive(props);
+    return {
+      classes: computed(() => ({
+        'button': true,
+        'button--primary': props.primary,
+        'button--secondary': !props.primary,
+        [`button--${props.size || 'medium'}`]: true,
+      })),
+      style: computed(() => ({
+        backgroundColor: props.backgroundColor,
+      })),
+      onClick() {
+        emit('click');
+      }
+    }
+  },
+});
+</script>
+
+<template>
+  <button type="button" :class="classes" @click="onClick" :style="style">{{ label }}</button>
+</template>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,7 +1,21 @@
 <script setup lang="ts">
-import Main from '../../components/Main/index.vue'
+import Button from '../../components/Button/index.vue'
+import { useI18n } from 'vue-i18n'
+const { t } = useI18n();
 </script>
 
 <template>
-  <Main />
+  <div class="wrapper">
+    <h1 class="title">{{ t('intro') }}</h1>
+    <Button
+      :primary="true"
+      :label="t('button.go')"
+      :size="'large'"
+      :backgroundColor="'#5B21B6'"
+    />
+  </div>
 </template>
+
+<style lang="scss">
+@import './scss/base.scss'
+</style>
